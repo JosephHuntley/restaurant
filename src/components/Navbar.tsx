5b2755ff94fc2bc31ec6d8e56a41b5cca51b6f4d
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-25 19:29:11 -0500

    Fixed Responsive Issue with Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,41 +6,44 @@ import NavMenu from './NavMenu';
 
 function Navbar() {
 	return (
-		<nav className='w-full flex md:px-[180px] px-6 sm:pt-[56px] pt-6 justify-between navbar'>
-			<div className='flex items-end'>
-				<img
-					src={logo}
-					alt='Logo'
-				/>
-				<p className='font-bold pl-[12px] pt-[28px] text-xl'>restaurant</p>
-			</div>
-			<ul className=' md:flex hidden items-end flex-row '>
-				{navLinks.map((nav: { title: string; id: string }, index: number) => (
-					<li
-						key={nav.id}
-						className={`font-normal cursor font-size-[16px] leading-7 font-inter text-dimWhite  ${
-							index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
-						}`}>
-						<a href={`#${nav.id}`}>{nav.title}</a>
-					</li>
-				))}
-			</ul>
-			<Button styles='bg-primary md:flex hidden leading-7 max-w-[167px]'>
-				Book a table
-			</Button>
-			<NavMenu />
-
+		<nav className=''>
 			{/* Rounded lines surrounding logo (Eclipses) */}
-			<div className='left-[-548px] top-[-452px] circle rounded-full md:flex ' />
-			<div className='left-[-604px] top-[-353.84px] circle rounded-full rotate-[17.41deg]' />
-			<div className='left-[-450px] top-[-428px] circle rounded-full' />
+			<div className='h-0 w-0 '>
+				<div className='left-[-600px] top-[-500px] circle rounded-full relative' />
+				<div className='left-[-550px] top-[-1500px] circle rounded-full relative rotate-12' />
+				<div className='left-[-500px] top-[-2350px] circle rounded-full relative' />
+				{/* Text Circle*/}
+				<img
+					src={circleText}
+					alt='Foods Delicious'
+					className=' w-[104px] h-[104px] top-[250px] left-[75px] absolute md:inline hidden'
+				/>
+			</div>
 
-			{/* Text Circle*/}
-			<img
-				src={circleText}
-				alt='Foods Delicious'
-				className='absolute md:flex hidden w-[104px] h-[104px] top-[275px] left-[119px]'
-			/>
+			<div className='md:justify-between flex w-full  md:px-[100px] px-6 md:pt-[56px] pt-6'>
+				<div className='flex items-end'>
+					<img
+						src={logo}
+						alt='Logo'
+					/>
+					<p className='font-bold pl-[12px] pt-[28px] text-xl'>restaurant</p>
+				</div>
+				<ul className=' md:flex hidden items-end flex-row '>
+					{navLinks.map((nav: { title: string; id: string }, index: number) => (
+						<li
+							key={nav.id}
+							className={`font-normal cursor font-size-[16px] leading-7 font-inter text-dimWhite  ${
+								index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+							}`}>
+							<a href={`#${nav.id}`}>{nav.title}</a>
+						</li>
+					))}
+				</ul>
+				<Button styles='bg-primary md:flex hidden leading-7 max-w-[167px]'>
+					Book a table
+				</Button>
+				<NavMenu />
+			</div>
 		</nav>
 	);
 }
